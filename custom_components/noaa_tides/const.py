87ba4e5bfb89df3d8fc9741bc4011f582953d914
Flab <118143714+Flight-Lab@users.noreply.github.com>
2025-02-04 23:41:35 -0500

    add ndbc sensors

--- custom_components/noaa_tides/const.py
+++ custom_components/noaa_tides/const.py
@@ -1,10 +1,3 @@
-"""Constants for the NOAA Tides integration."""
-
-DOMAIN = "noaa_tides"
-
-VERSION = "0.3.2"
-
-# Configuration fields
 CONF_STATION_ID = "station_id"
 CONF_TIMEZONE = "timezone"
 CONF_UNIT_SYSTEM = "unit_system"
@@ -13,15 +6,20 @@ DEFAULT_NAME = "NOAA Tides"
 DEFAULT_TIMEZONE = "lst_ldt"
 DEFAULT_UNIT_SYSTEM = "imperial"
 
-
 # Platforms supported by the integration
 PLATFORMS = ["sensor"]
 
 
 # Mapping of internal sensor keys to friendly labels
 SENSOR_OPTIONS = {
+    # NOAA sensor options:
     "tide_state": "Tide State",
     "measured_level": "Measured Level",
     "water_temp": "Water Temperature",
     "air_temp": "Air Temperature",
+    # NDBC sensor options:
+    "wind_speed": "Wind Speed",
+    "wind_direction": "Wind Direction",
+    "wave_height": "Wave Height",
+    "barometric_pressure": "Barometric Pressure",
 }
